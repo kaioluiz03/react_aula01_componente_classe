4ee9b3fb4e3e0961551833442fa22bc377a636fe
Kaio <luizkaio9815@gmail.com>
2021-10-13 20:07:00 -0300

    Cria os arquivos da atividade sobre componentes de classe

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ClassComponent from './components/ClassComponent';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <ClassComponent/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ClassComponent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './CComponentStyle.css';
+
+
+class ClassComponent extends React.Component {
+    render(){
+        return(
+
+            <div className="container">
+                <h1>Olá Mundo React</h1>
+                <p>Esse é o meu primeiro componente de Classe ☜(ﾟヮﾟ☜)</p>
+            </div>
+
+        )
+    }
+}
+export default ClassComponent;
